e11077b7cc523b3593848ba5c4341e475503dbf3
MyManVan <kcjackkelly@gmail.com>
2020-04-22 19:15:26 -0500

    Added dom selectors in render js

---
+++ src/render.js
@@ -0,0 +1,4 @@
+const stopVideo = document.getElementById("stopVideo");
+const startVideo = document.getElementById("startVideo");
+const selectVideo = document.getElementById("selectVideo");
+const videoPreview = document.getElementById("videoPreview");
