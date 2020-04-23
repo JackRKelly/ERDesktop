ef70ff6fdbba9a1ce8b066792265f218a18c93f4
MyManVan <kcjackkelly@gmail.com>
2020-04-22 19:16:12 -0500

    Added electron desktop capture

--- src/render.js
+++ src/render.js
@@ -2,3 +2,11 @@ const stopVideo = document.getElementById("stopVideo");
 const startVideo = document.getElementById("startVideo");
 const selectVideo = document.getElementById("selectVideo");
 const videoPreview = document.getElementById("videoPreview");
+
+selectVideo.addEventListener("click", () => {
+  getSources();
+});
+
+const { desktopCapturer, remote } = require("electron");
+
+const { Menu } = remote;
